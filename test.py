c75b3978e2016ce510e7fba93f91c2a1c7b36924
jerry-ban <jlban@hotmail.com>
2018-03-16 18:04:35 -0400

    progress

--- test.py
+++ test.py
@@ -7,6 +7,13 @@ import logging
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+from mpl_toolkits.basemap import Basemap
+from matplotlib import cm
+
+
+#import gmplot
+
 from sklearn.preprocessing import MinMaxScaler
 
 file_trip = "trip.csv"
@@ -65,16 +72,65 @@ dfc2= df_chart[df_chart["is_weekend"]=="weekend"].pivot("date", "subscription_ty
 dfc2.plot.area(ax=ax2, title ="weekend", color=pal)
 
 
-Station_trip = pd.merge(Trip, Station,how = "inner", left_on="start_station_id", right_on="id", )
+Station_trip = pd.merge(Trip, Station,how = "inner", left_on="start_station_id", right_on="id", suffixes = ["",".y"])
 #Station_trip_count.groupby(["date", "subscription_type", "city" ])["count"].unstack("city")  #pivot with multiple index
 
 logging.info("city wise charts")
 Station_trip_count = Station_trip.groupby(["date", "subscription_type", "city" ]).size().reset_index(name="count")
-g = sns.FacetGrid(Station_trip_count, col='city', hue='subscription_type', col_wrap=4, sharey=False, )
+g = sns.FacetGrid(Station_trip_count, col='city', hue='subscription_type', col_wrap=3, sharey=False, )
 g = g.map(plt.plot, 'date', 'count')
 g = g.map(plt.fill_between, 'date', 'count', alpha=0.2).set_titles("{col_name} city")
 g = g.set_titles("{col_name}")
 plt.subplots_adjust(top=0.92)
 g.add_legend()
 
-logging.info("city wise charts")
+logging.info("city-weekday charts")
+Station_wday_count = Station_trip.groupby(["start_wday", "city" ]).size().reset_index(name="count")
+wday_map = {"Monday":1, "Tuesday":2, "Wednesday":3, "Thursday": 4, "Friday":5, "Saturday":6, "Sunday": 7}
+Station_wday_count["wday"] = Station_wday_count["start_wday" ].map(wday_map)
+city_map = {'Mountain View':1, 'Palo Alto':2, 'Redwood City':3, 'San Francisco':4, 'San Jose':5}
+Station_wday_count["city_num"] = Station_wday_count["city" ].map(city_map)
+plt.figure()
+ax = plt.gca()
+Station_wday_count.plot.scatter(x="wday", y="city_num", s=Station_wday_count["count"]/10, title = "city vs weekday" )
+ax.set_xticklabels(Station_wday_count["start_wday"])
+ax.yaxis.set_ticks(np.arange(1, 5.5, 1))
+ax.set_yticklabels(Station_wday_count["city"])
+
+logging.info("show trip in geographic map(geomap)")
+Trip_map = Station_trip.groupby(["long", "lat", "zip_code"]).size().reset_index(name="count")
+
+lb_long, lb_lat, rt_long, rt_lat = -122.4990, 37.31072, -121.7800, 37.88100
+lon_c = float(lb_long + rt_long) / 2.0
+lat_c = float(lb_lat + rt_lat) / 2.0
+
+
+fig = plt.figure()
+ax = plt.gca()
+m = Basemap(resolution='i', lon_0=lon_c, lat_0=lat_c, llcrnrlon=lb_long,llcrnrlat=lb_lat,urcrnrlon= rt_long,urcrnrlat=rt_lat)
+x, y = m(Trip_map['long'].values, Trip_map['lat'].values)
+m.scatter(x, y,markersize=Trip_map['count'].values)
+m.plot(x,y, )
+m.drawcounties()
+m.drawstates()
+m.drawcounties()
+m.fillcontinents()
+
+
+Station_trip.rename(columns={ "lat": "start_lat", "long": "start_long"}, inplace=True)
+
+End_station_trip = pd.merge(Trip, Station, how = "inner", left_on= "end_station_id", right_on ="id", suffixes=["", ".y"])
+End_station_trip.rename(columns={"lat": "end_lat", "long": "end_long"}, inplace=True)
+
+Station_trip_sf = Station_trip[Station_trip["city"]=="San Francisco"]
+Road_df = pd.merge(Station_trip_sf[["id","start_station_id", "start_lat", "start_long","city"]], End_station_trip[["id","end_station_id","end_lat", "end_long","city"]]
+                   , how="inner", on = "id", suffixes=[".x",".y"] )
+
+
+
+
+Road_df_count=Road_df.groupby("")
+Road_df.plot.scatter(x="start_long", y="start_lat",
+    #s=Road_df['count']/10#, label="population",  c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True, alpha=0.4 , figsize=(10,7),
+                     )
+plt.show()
